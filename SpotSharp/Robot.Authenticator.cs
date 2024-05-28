c3960fbd73e3cbc849422ce33f024235642d56a7
Haoyu Jia <57647667+OldVincent@users.noreply.github.com>
2024-05-28 20:18:29 +0900

    Add a class to do connection and authentication.

---
+++ SpotSharp/Robot.Authenticator.cs
@@ -0,0 +1,84 @@
+﻿using Bosdyn.Api;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Metadata = Grpc.Core.Metadata;
+
+namespace SpotSharp;
+
+public partial class Robot
+{
+    private class Authenticator
+    {
+        public string ClientName;
+
+        private readonly AuthService.AuthServiceClient _service;
+        private string _token = string.Empty;
+        private string _authorization = string.Empty;
+
+        public DateTime Timestamp { get; private set; }
+        
+        private void BindToken(string token)
+        {
+            _token = token;
+            _authorization = $"Bearer {token}";
+        }
+        
+        private Authenticator(
+            AuthService.AuthServiceClient service, string client, string token,
+            DateTime timestamp)
+        {
+            ClientName = client;
+            _service = service;
+            BindToken(token);
+            Timestamp = timestamp;
+        }
+
+        public async Task<GetAuthTokenResponse.Types.Status> Update()
+        {
+            var response = await _service.GetAuthTokenAsync(
+                new GetAuthTokenRequest()
+                {
+                    Header = new RequestHeader()
+                    {
+                        ClientName = ClientName,
+                        RequestTimestamp = DateTime.UtcNow.ToTimestamp()
+                    },
+                    Token = _token
+                });
+            if (response.Status == GetAuthTokenResponse.Types.Status.Ok)
+                BindToken(response.Token);
+            return response.Status;
+        }
+
+        public static async Task<Authenticator> Create(
+            GrpcChannel channel, string client, string username, string password)
+        {
+            var service = new AuthService.AuthServiceClient(channel);
+            var response = await service.GetAuthTokenAsync(new GetAuthTokenRequest
+            {
+                Header = new RequestHeader()
+                {
+                    ClientName = client,
+                    RequestTimestamp = DateTime.UtcNow.ToTimestamp()
+                },
+                Username = username,
+                Password = password
+            });
+
+            if (response.Status != GetAuthTokenResponse.Types.Status.Ok)
+                throw new ApplicationException($"Failed to authenticate: {response.Status}.");
+
+            return new Authenticator(service, client, response.Token, 
+                response.Header.ResponseTimestamp.ToDateTime());
+        }
+
+        private Task Intercept(AuthInterceptorContext context, Metadata metadata)
+        {
+            metadata.Add("authorization", _authorization);
+            return Task.CompletedTask;
+        }
+
+        public CallCredentials Credential => CallCredentials.FromInterceptor(Intercept);
+    }
+}
---
+++ SpotSharp/Robot.cs
@@ -0,0 +1,81 @@
+﻿using Bosdyn.Api;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Logging;
+using Type = System.Type;
+
+namespace SpotSharp;
+
+public partial class Robot : IDisposable
+{
+    public readonly string ClientName;
+
+    public readonly Uri Address;
+
+    private readonly Authenticator _authenticator;
+
+    private readonly ILoggerFactory? _loggers;
+
+    private Robot(Uri address, string client, Authenticator authenticator, ILoggerFactory? loggers)
+    {
+        Address = address;
+        ClientName = client;
+        _authenticator = authenticator;
+        _loggers = loggers;
+    }
+
+    public void Dispose()
+    {
+        _services.Clear();
+        foreach (var (_, channel) in _channels)
+        {
+            channel.Dispose();
+        }
+        _channels.Clear();
+    }
+
+    private readonly Dictionary<string, GrpcChannel> _channels = new();
+    
+    /// <summary>
+    /// Get an authenticated channel according to the specific authority.
+    /// </summary>
+    /// <param name="authority">Authority, which is the address of the service.</param>
+    /// <returns>Channel for the specific authority.</returns>
+    public GrpcChannel GetChannel(string authority)
+    {
+        if (_channels.TryGetValue(authority, out var channel))
+            return channel;
+        channel = CreateChannel(Address, authority, _authenticator, _loggers);
+        _channels[authority] = channel;
+        return channel;
+    }
+
+    public async Task<GetAuthTokenResponse.Types.Status> UpdateToken()
+        => await _authenticator.Update();
+
+    /// <summary>
+    /// Create a new request header according to the client name and current time.
+    /// </summary>
+    public RequestHeader Header => new RequestHeader()
+    {
+        ClientName = ClientName,
+        RequestTimestamp = DateTime.UtcNow.ToTimestamp()
+    };
+
+    private readonly Dictionary<Type, ClientBase> _services = new();
+
+    public TService GetService<TService>(string authority = "api.spot.robot") where TService : ClientBase
+    {
+        var type = typeof(TService);
+        if (!_services.TryGetValue(type, out var service))
+        {
+            service = (TService?)Activator.CreateInstance(typeof(TService), [GetChannel(authority)])
+                      ?? throw new ApplicationException(
+                          $"Can not instantiate service client #{typeof(TService)}.");
+            _services[type] = service;
+        }
+
+        return (TService)service;
+    }
+}
